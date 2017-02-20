a941d251c216c6ec1b71a866b169613f1807a8f8
Chuck Short <chuck.short@canonical.com>
2017-02-20 10:17:55 -0500

    Imported Upstream version 10.0.0~rc2

--- neutron/agent/linux/ip_lib.py
+++ neutron/agent/linux/ip_lib.py
@@ -205,15 +205,6 @@ class IPWrapper(SubProcessBase):
         self._as_root([], 'link', ('add', name, 'type', 'dummy'))
         return IPDevice(name, namespace=self.namespace)
 
-    def add_ifb(self, name):
-        """Create a Linux IFB type interface with the given name."""
-        self._as_root([], 'link', ('add', name, 'type', 'ifb'))
-        return IPDevice(name, namespace=self.namespace)
-
-    def del_ifb(self, name):
-        """Delete a Linux IFB type interface with the given name."""
-        self._as_root([], 'link', ('del', name))
-
     def ensure_namespace(self, name):
         if not self.netns.exists(name):
             ip = self.netns.add(name)
@@ -475,7 +466,10 @@ class IpRuleCommand(IpCommandBase):
     def add(self, ip, **kwargs):
         ip_version = get_ip_version(ip)
 
-        kwargs.update({'from': ip})
+        # In case if we need to add in a rule based on incoming
+        # interface we don't need to pass in the ip.
+        if not kwargs.get('iif'):
+            kwargs.update({'from': ip})
         canonical_kwargs = self._make_canonical(ip_version, kwargs)
 
         if not self._exists(ip_version, **canonical_kwargs):
